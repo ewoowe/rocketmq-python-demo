e767dc5ea5349f1c12e38f6d47045f623ad6133e
public2 <public2@ictnj.ac.cn>
2023-03-28 17:36:44 +0800

    Changes

--- Consumer6NM1.py
+++ Consumer6NM1.py
@@ -7,12 +7,11 @@ from NodeDiscoverMessage import json2NodeDiscoverMessage
 
 
 def callback(msg):
-    print(msg.body)
     node = json.loads(msg.body, object_hook=json2NodeDiscoverMessage)
     print('node: nodeId=%s, ip=%s, type=%s' % (node.nodeId, node.ip, node.type))
     return ConsumeStatus.CONSUME_SUCCESS
 
-consumer = PushConsumer('NM4')
+consumer = PushConsumer('nm1')
 consumer.set_name_server_address('127.0.0.1:9876')
 consumer.subscribe('NodeDiscovery', callback)
 consumer.start()
---
+++ Consumer7NM3.py
@@ -0,0 +1,24 @@
+import json
+import time
+
+from rocketmq.client import PushConsumer, ConsumeStatus
+from rocketmq.ffi import MessageModel
+
+from NodeDiscoverMessage import json2NodeDiscoverMessage
+
+
+def callback(msg):
+    node = json.loads(msg.body, object_hook=json2NodeDiscoverMessage)
+    print('node: nodeId=%s, ip=%s, type=%s' % (node.nodeId, node.ip, node.type))
+    return ConsumeStatus.CONSUME_SUCCESS
+
+consumer = PushConsumer('nm3')
+consumer.set_name_server_address('127.0.0.1:9876')
+consumer.set_message_model(MessageModel.BROADCASTING)
+consumer.subscribe('NodeDiscovery', callback)
+consumer.start()
+
+while True:
+    time.sleep(3600)
+
+consumer.shutdown()
